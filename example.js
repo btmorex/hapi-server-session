4e5e41c266dc07f6b95b651c3155ed111e3f3002
Avery Fay <avery@shadypixel.com>
2015-07-14 17:41:44 -0700

    add example

---
+++ example.js
@@ -0,0 +1,29 @@
+var hapi = require('hapi');
+
+var server = new hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  address: '127.0.0.1',
+  port: 8000
+});
+
+server.register({
+  register: require('./index.js'),
+  options: {
+    cookieOptions: {
+      isSecure: false
+    }
+  }
+}, function (err) { if (err) throw err; });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    request.session.views = request.session.views + 1 || 1;
+    reply('Views: ' + request.session.views);
+  }
+});
+
+server.start();
